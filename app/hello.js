be108bb46587b8273bd0d845b64887fca4692693
Eric Smith <esmith@8thlight.com>
2017-04-12 08:03:09 -0500

    Initial Kata State

--- app/hello.js
+++
@@ -1,5 +0,0 @@
-export default {
-  world() {
-    console.log("hello world");
-  }
-};
--- specs/app/hello-spec.js
+++
@@ -1,7 +0,0 @@
-import Hello from "app/hello";
-
-describe("Hello", () => {
-  it("is currently failing", () => {
-    expect(true).toBe(false);
-  });
-});
